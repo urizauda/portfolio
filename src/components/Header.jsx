b52f11a6fcc3f10e538266871bcb00450860a2b9
Uri Zauda <87427105+urizauda@users.noreply.github.com>
2023-01-21 21:03:02 +0200

    navbar links setup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 export default function Header() {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <div>
+            <header className="text-3xl font-bold underline">
+                <a href='/'>Uri</a>
+                {/* Desktop Nav */}
+                <nav>
+                    <ul>
+                        <li><a href='/#about'>About</a></li>
+                        <li><a href='/#projects'>Projects</a></li>
+                        <li><a href='/#contact'>Contact</a></li>
+                        <li><a href='/#resume -link'>Resume</a></li>
+                    </ul>
+                </nav>
+
+                {/* Mobile Nav */}
+                <nav>
+                    <ul>
+                        <li><a href='/#about'>About</a></li>
+                        <li><a href='/#projects'>Projects</a></li>
+                        <li><a href='/#contact'>Contact</a></li>
+                        <li><a href='/#resume -link'>Resume</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    )
 }
