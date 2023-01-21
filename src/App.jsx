0aa5fbc7bfd9c0c5c2d9319dce0597ef057d17bc
Uri Zauda <87427105+urizauda@users.noreply.github.com>
2023-01-21 19:49:02 +0200

    components setup

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import Header from './components/Header';
+import About from './components/About';
+import Hero from './components/Hero';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
+import Contact from './components/Contact';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header />
+      <About />
+      <Hero />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Contact.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Contack() {
+  return (
+    <div>Contact</div>
+  )
+}
